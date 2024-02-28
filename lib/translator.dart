9bf0333ebe889e3701fd6f23f5cb1539a1a80de7
Smart brain idea LLC <118807513+sbidotdev@users.noreply.github.com>
2024-02-28 19:16:03 +0300

    28 - 2 - 2024 update

--- lib/translator.dart
+++ lib/translator.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:signapp/camera.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class Translator extends StatefulWidget {
   const Translator({super.key});
@@ -16,12 +18,20 @@ class _TranslatorState extends State<Translator> {
   String? imgurl = "";
   final TextEditingController TranslateTextBox = TextEditingController();
   late FlutterTts flutterTts;
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  bool _speechStatus = false;
 
   //FUN
   initTts() {
     flutterTts = FlutterTts(); // create new object
     flutterTts.setLanguage("ar"); //set language to arabic
     _setAwaitOptions();
+  }
+
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
   }
 
   Future _setAwaitOptions() async {
@@ -40,12 +50,51 @@ class _TranslatorState extends State<Translator> {
     }
   }
 
+  //Voice Recogintion
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      final docRef = FirebaseFirestore.instance
+          .collection("arabic_to_sign")
+          .doc("nBnlKQ7Hn8NRKRjq6k57");
+      docRef.get().then(
+        (res) {
+          print("WORD CATCH >>>> " + result.recognizedWords);
+          if (result.recognizedWords.isEmpty) {
+            print("Error image");
+          } else {
+            if (res.data()!.containsValue(result.recognizedWords.toString())) {
+              print("sorry the value not contain the list in firebase !");
+            } else {
+              setState(() {
+                imgurl = "";
+                imgurl = res[result.recognizedWords];
+                print(imgurl);
+              });
+            }
+          }
+        },
+        onError: (e) => print("Error completing: $e"),
+      );
+    });
+  }
+
   //INIT
   @override
   initState() {
     // TODO: implement initState
     super.initState();
     initTts(); // init listen button
+    _initSpeech();
   }
 
   @override
@@ -75,134 +124,199 @@ class _TranslatorState extends State<Translator> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // ***** TEXT SECTION ***** //
-          Container(
-            height: 250,
-            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            alignment: Alignment.topLeft,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade400,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: TranslateTextBox,
-                keyboardType: TextInputType.multiline,
-                maxLines: 20,
-                textDirection: TextDirection.rtl,
-                decoration: InputDecoration(
-                  hintText: "نص الترجمة...",
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 180,
+                    alignment: Alignment.topLeft,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        controller: TranslateTextBox,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 20,
+                        textDirection: TextDirection.rtl,
+                        decoration: InputDecoration(
+                          hintText: "نص الترجمة...",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10.0,
+                      right: 10.0,
+                      top: 20.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (TranslateTextBox.text == "") {
+                              print("لايوجد نص للترجمة !");
+                            } else {
+                              //Get The Document and return same text the url image
+                              final docRef = FirebaseFirestore.instance
+                                  .collection("arabic_to_sign")
+                                  .doc("nBnlKQ7Hn8NRKRjq6k57");
+                              docRef.get().then(
+                                (res) {
+                                  if (res[TranslateTextBox.text].toString() ==
+                                      "") {
+                                    print("Error image");
+                                  } else {
+                                    setState(() {
+                                      imgurl = "";
+                                      imgurl = res[TranslateTextBox.text];
+                                      print(imgurl);
+                                    });
+                                  }
+                                },
+                                onError: (e) => print("Error completing: $e"),
+                              );
+                            }
+                          },
+                          child: Container(
+                            width: 80,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.translate,
+                                  size: 24,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text(
+                                  "ترجم للإشارة",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              print(TranslateTextBox.text);
+                              _speak();
+                            });
+                          },
+                          child: Container(
+                            width: 80,
+                            height: 50,
+                            margin: const EdgeInsets.only(left: 10, right: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.campaign_rounded,
+                                  size: 24,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text(
+                                  "إستمع للترجمة",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              InkWell(
+                onTap: () {
+                  if (_speechStatus == true) {
+                    _stopListening();
+                    setState(() {
+                      _speechStatus = false;
+                    });
+                  } else {
+                    _startListening();
+                    setState(() {
+                      _speechStatus = true;
+                    });
+                  }
+                },
+                child: Container(
+                  height: 320,
+                  width: 180,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(right: 10, bottom: 0),
+                  decoration: BoxDecoration(
+                    color:
+                        _speechStatus == true ? Colors.green : Colors.lightBlue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.mic,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      _speechStatus == true
+                          ? Text(
+                              "تحدث الأن ...",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            )
+                          : Text(
+                              "اضغط للتحدث",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                    ],
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 10.0,
-              right: 10.0,
-              top: 20.0,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {
-                    if (TranslateTextBox.text == "") {
-                      print("لايوجد نص للترجمة !");
-                    } else {
-                      //Get The Document and return same text the url image
-                      final docRef = FirebaseFirestore.instance
-                          .collection("arabic_to_sign")
-                          .doc("nBnlKQ7Hn8NRKRjq6k57");
-                      docRef.get().then(
-                        (res) {
-                          if (res[TranslateTextBox.text].toString() == "") {
-                            print("Error image");
-                          } else {
-                            setState(() {
-                              imgurl = "";
-                              imgurl = res[TranslateTextBox.text];
-                              print(imgurl);
-                            });
-                          }
-                        },
-                        onError: (e) => print("Error completing: $e"),
-                      );
-                    }
-                  },
-                  child: Container(
-                    width: 100,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.translate,
-                          size: 24,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Text(
-                          "ترجم للإشارة",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print(TranslateTextBox.text);
-                      _speak();
-                    });
-                  },
-                  child: Container(
-                    width: 100,
-                    height: 50,
-                    margin: const EdgeInsets.only(left: 10, right: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.campaign_rounded,
-                          size: 24,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Text(
-                          "إستمع للترجمة",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+
           // ***** IMAGE SECTION ***** //
           Container(
             height: 230,
